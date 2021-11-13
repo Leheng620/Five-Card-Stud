53cf02ea017cfe139a7cc9a346e1aae71a589365
Jui Po Hung <juipohun@usc.edu>
2021-11-13 14:02:32 -0500

    Quick Init

--- five_card_stub/node.py
+++ five_card_stub/node.py
@@ -1,2 +1,6 @@
-class Node:
-    
+class Node():
+    def __init__(self, children=None, parent=None, win_cnt=0, expl_cnt=0):
+        self.children = children
+        self.parent = parent
+        self.win_cnt = win_cnt
+        self.expl_cnt = expl_cnt
